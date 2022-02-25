7686cd3d995d97e36a825857d1d042f6aef71300
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2022-02-25 23:36:34 +0900

    Cleaned up title scene

---
+++ Assets/Scripts/UI/Introstuff/GoToJapan.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GoToJapan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Image>().color= Color.clear;
+        gameObject.GetComponentInChildren<TMP_Text>().color = Color.clear;
+        StartCoroutine("FadeIn");
+    }
+    private IEnumerator FadeIn(){
+        yield return new WaitForSeconds(5);
+        this.gameObject.GetComponent<Image>().color = Color.white;
+        gameObject.GetComponentInChildren<TMP_Text>().color = Color.black;
+    }
+    public void GoToNext(){
+        SceneManager.LoadScene("NichTest");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
