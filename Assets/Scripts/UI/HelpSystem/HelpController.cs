50e1044c53422531fafb16ad5da8310c5d270a95
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2022-03-11 14:59:23 +0900

    w

--- Assets/Scripts/UI/HelpSystem/HelpController.cs
+++ Assets/Scripts/UI/HelpSystem/HelpController.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HelpController : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    public TMP_Text helpText;
+    private string Text;
+    private string Score;
+    private int score;
     void OnEnable(){
-
+        PropClicked.propClicked += SetText;
     }
     void OnDisable(){
-        
+        PropClicked.propClicked -= SetText;
+    }
+    void SetText(string text){
+        if(text == "Tree"){
+            Text = "Lightning has struck this tree, the Raijuu must have been here!";
+            score++;
+        }
+        if(text == "Footprint"){
+            Text = "These tracks are still fresh, the Raijuu must be close!";
+            score++;
+        }
+        if(score >= 2 ){
+            score = 2;
+            Score = "2/2";
+        }
+        else if(score == 1){
+            Score = "1/2";
+        }
+        else{
+            Score = "0/2";
+        }
+
     }
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Yokai Scripts/RaijuuScripts/PropClicked.cs
+++ Assets/Scripts/Yokai Scripts/RaijuuScripts/PropClicked.cs
@@ -5,12 +5,13 @@ using System;
 
 public class PropClicked : MonoBehaviour
 {
-    public static Action propClicked;
+    public static Action<string> propClicked;
+    public string propName;
     private bool done = true;
     void OnMouseDown(){
         Debug.Log("done.ToString()");
         if(done){
-        propClicked?.Invoke();
+        propClicked?.Invoke("propName");
         this.GetComponent<TooltipTrigger>().isActive = false;
         done = false;
         Debug.Log(done.ToString());
