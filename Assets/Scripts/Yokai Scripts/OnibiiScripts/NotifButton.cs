92c787b8c8d3ba0e0388d5202f98339cf7612db0
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2022-03-13 17:33:18 +0900

    Notif Sys

---
+++ Assets/Scripts/Yokai Scripts/OnibiiScripts/NotifButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NotifButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator mAni;
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        //Output to console the GameObject's name and the following message
+        Debug.Log("Cursor Entering " + name + " GameObject");
+        mAni.SetBool("isActive",true);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        //Output the following message with the GameObject's name
+        Debug.Log("Cursor Exiting " + name + " GameObject");
+        mAni.SetBool("isActive",false);
+        //StartCoroutine("StayAWhile");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAni = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
