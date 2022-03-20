e3a29e4676b234ab511789a2d17d6e29636f22b4
Vic48 <97833997+Vic48@users.noreply.github.com>
2022-03-20 20:12:10 +0800

    Collection Book control update

--- Assets/Scripts/CollectionBookControl.cs
+++ Assets/Scripts/CollectionBookControl.cs
@@ -7,9 +7,13 @@ public class CollectionBookControl : MonoBehaviour
 {
     [SerializeField] GameObject collectionBook;
     [SerializeField] GameObject descriptionText;
-    [SerializeField] GameObject picContainer;
-    [SerializeField] GameObject yokaiNameContainer;
+    [SerializeField] GameObject yokaiPic;
+    [SerializeField] GameObject loreboxText;
+    [SerializeField] GameObject yokaiName;
     [SerializeField] GameObject locales;
+    [SerializeField] GameObject buttonRight;
+    [SerializeField] GameObject buttonLeft;
+    [SerializeField] GameObject pageNumber;
 
     private string yokaiNotFoundPic = "yokai_notFound";
     void OnDisable(){
@@ -23,42 +27,42 @@ public class CollectionBookControl : MonoBehaviour
     void Update()
     {
         // onClick Left yokai name, open the right pannel to show the description
-        onClickYokaiName();
+        // onClickYokaiName();
     }
 
-    private void onClickYokaiName() {
-        //If the left mouse button is clicked.
-        if (collectionBook.activeSelf && Input.GetMouseButtonDown(0))
-        {
-            //Get the mouse position on the screen and send a raycast into the game world from that position.
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+    // private void onClickYokaiName() {
+    //     //If the left mouse button is clicked.
+    //     if (collectionBook.activeSelf && Input.GetMouseButtonDown(0))
+    //     {
+    //         //Get the mouse position on the screen and send a raycast into the game world from that position.
+    //         Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //         RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-            //If something was hit, the RaycastHit2D.collider will not be null.
-            if (hit.collider != null && hit.collider.gameObject.tag == "YokaiAvatar")
-            {
-                string yokaiName = hit.collider.name;
-                bool isCatched = PlayManager.Instance.GetCaughtYokai(
-                    (PlayManager.QuestName)System.Enum.Parse(
-                        typeof(PlayManager.QuestName), yokaiName)
-                );
-                if (!isCatched) {
-                    // If not catched, show not found
-                    yokaiNameContainer.GetComponent<Text>().text = "unkown";
-                    descriptionText.GetComponent<Text>().text = "Not Found!";
-                    picContainer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(yokaiNotFoundPic);;
-                } else {
-                    // If catched, show infomation
-                    string description = YokaiControl.Instance.getYokaiDescription(yokaiName);
-                    string picUrl = YokaiControl.Instance.getYokaiPic(yokaiName);
-                    var yokaiPicSprite = Resources.Load<Sprite>(picUrl);
-                    yokaiNameContainer.GetComponent<Text>().text = yokaiName;
-                    if (!description.Equals("Default")) {
-                        descriptionText.GetComponent<Text>().text = description;
-                        picContainer.GetComponent<SpriteRenderer>().sprite = yokaiPicSprite;
-                    }
-                }
-            }
-        }
-    }
+    //         //If something was hit, the RaycastHit2D.collider will not be null.
+    //         if (hit.collider != null && hit.collider.gameObject.tag == "YokaiAvatar")
+    //         {
+    //             string yokaiName = hit.collider.name;
+    //             bool isCatched = PlayManager.Instance.GetCaughtYokai(
+    //                 (PlayManager.QuestName)System.Enum.Parse(
+    //                     typeof(PlayManager.QuestName), yokaiName)
+    //             );
+    //             if (!isCatched) {
+    //                 // If not catched, show not found
+    //                 yokaiNameContainer.GetComponent<Text>().text = "unkown";
+    //                 descriptionText.GetComponent<Text>().text = "Not Found!";
+    //                 picContainer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(yokaiNotFoundPic);;
+    //             } else {
+    //                 // If catched, show infomation
+    //                 string description = YokaiControl.Instance.getYokaiDescription(yokaiName);
+    //                 string picUrl = YokaiControl.Instance.getYokaiPic(yokaiName);
+    //                 var yokaiPicSprite = Resources.Load<Sprite>(picUrl);
+    //                 yokaiNameContainer.GetComponent<Text>().text = yokaiName;
+    //                 if (!description.Equals("Default")) {
+    //                     descriptionText.GetComponent<Text>().text = description;
+    //                     picContainer.GetComponent<SpriteRenderer>().sprite = yokaiPicSprite;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
