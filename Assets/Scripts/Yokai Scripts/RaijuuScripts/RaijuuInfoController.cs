207ed428f6fe91af7b9529ac149f886399253e37
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2022-03-21 01:45:34 +0900

    Raijuu fixed

--- Assets/Scripts/Yokai Scripts/RaijuuScripts/RaijuuInfoController.cs
+++ Assets/Scripts/Yokai Scripts/RaijuuScripts/RaijuuInfoController.cs
@@ -11,6 +11,9 @@ public class RaijuuInfoController : MonoBehaviour
     private bool showSecond= false;
     public static Action showingPane;
     public static Action paneClosed;
+    public Camera cam;
+    public GameObject raijuu1,raijuu2;
+    private bool rai1= false, rai2=false;
     void OnEnable(){
         RaijuuCapture.raijuuCaught+= Judge;
     }
@@ -31,15 +34,19 @@ public class RaijuuInfoController : MonoBehaviour
     }
     void ShowSecondPanel(){
         panel2.enabled=true;
+        rai2=true;
         showingPane?.Invoke();
     }
     void ShowFirstPanel(){
         showSecond=true;
         panel1.enabled = true;
+        rai1=true;
         showingPane?.Invoke();
     }
     public void ClosePanel(){
         panel1.enabled=false;
+        cam.transform.position = new Vector3(0f,0f, -10);
+        cam.orthographicSize = 16.2f;
         paneClosed?.Invoke();
     }
     // Start is called before the first frame update
@@ -52,6 +59,21 @@ public class RaijuuInfoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(rai1){
+            cam.transform.position = new Vector3(176.2616f,-17.39878f, -10);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 6.1f, Time.deltaTime * 5);
+            cam.transform.position = new Vector3((raijuu1.GetComponent<Transform>().position.x-5) + 0.7f, raijuu1.GetComponent<Transform>().position.y, -10);
+            if(cam.orthographicSize < 6.5f){
+            rai1= false;
+            }
+        }
+        else if(rai2){
+            cam.transform.position = new Vector3(176.2616f,-17.39878f, -10);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 6.1f, Time.deltaTime * 5);
+            cam.transform.position = new Vector3((raijuu2.GetComponent<Transform>().position.x-5) + 0.7f, raijuu2.GetComponent<Transform>().position.y, -10);
+            if(cam.orthographicSize < 6.5f){
+            rai2= false;
+            }
+        }
     }
 }
