2ba1c76ae3276a78d69e60b33c580570f4d74b9e
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2022-03-21 00:46:05 +0900

    Updated Onibi Scene

--- Assets/Scripts/Yokai Scripts/OnibiiScripts/OnibiCapData.cs
+++ Assets/Scripts/Yokai Scripts/OnibiiScripts/OnibiCapData.cs
@@ -9,6 +9,11 @@ public class OnibiCapData : MonoBehaviour
     public static Action activeOniGroup;
     [SerializeField]
     private Canvas panel1,panel2;
+    [SerializeField]
+    private GameObject Onibi1, Onibi2;
+    [SerializeField]
+    private Camera cam;
+    bool showOni1= false, showOni2=false;
     void OnEnable(){
         Onibii.onibiiCaptured += ShowFirstPanel;
         OniGroup.oniGroupCaptured += ShowSecondPanel;
@@ -23,12 +28,16 @@ public class OnibiCapData : MonoBehaviour
     }
     void ShowSecondPanel(){
         panel2.enabled=true;
+        showOni2=true;
     }
     void ShowFirstPanel(string s){
         panel1.enabled = true;
+        showOni1 = true;
     }
     public void ClosePanel(){
         panel1.enabled=false;
+        cam.transform.position = new Vector3(100f,0f, -10);
+        cam.orthographicSize = 16.2f;
         activeOniGroup?.Invoke();
     }
     // Start is called before the first frame update
@@ -41,6 +50,21 @@ public class OnibiCapData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(showOni1){
+            cam.transform.position = new Vector3(176.2616f,-17.39878f, -10);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 4.1f, Time.deltaTime * 5);
+            cam.transform.position = new Vector3(Onibi1.GetComponent<Transform>().position.x + 0.7f, Onibi1.GetComponent<Transform>().position.y, -10);
+            if(cam.orthographicSize < 4.5f){
+            showOni1= false;
+            }
+        }
+        else if(showOni2){
+            cam.transform.position = new Vector3(176.2616f,-17.39878f, -10);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 4.1f, Time.deltaTime * 5);
+            cam.transform.position = new Vector3(Onibi2.GetComponent<Transform>().position.x + 0.7f, Onibi2.GetComponent<Transform>().position.y, -10);
+            if(cam.orthographicSize < 4.5f){
+            showOni2= false;
+            }
+        }
     }
 }
