47c6bb897df7b60d45f57f89d4f2f1811fc0ec46
Kitanae <kadokawa.kitanae@gmail.com>
2022-03-30 16:43:30 +0800

    credits and ea checks

---
+++ Assets/Scripts/UI/Introstuff/CreditsButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartCredits(){
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
